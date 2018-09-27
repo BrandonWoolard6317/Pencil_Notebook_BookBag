b4742d7c141ff2d6ae2ef28398a844c6bd8a0474
Brandon Woolard <brandon.woolard.2019@student.lhschools.org>
2018-09-27 15:42:53 -0400

    Made Loop

--- src/PNB.java
+++ src/PNB.java
@@ -11,11 +11,14 @@ public class PNB {
         String usersChoice;
         String objectName;
         String[] Object;
+        int Generator;
+        int loopGenerator;
+        int usersObject;
 
         //Initializing Variables
         input = new Scanner(System.in);
         Random PNB = new Random();
-        int Generator = PNB.nextInt(3);
+        Generator = PNB.nextInt(3);
 
         //Array for PNB
         Object = new String[3];
@@ -27,16 +30,14 @@ public class PNB {
         System.out.println("Let's play Pencil, Notebook, Book Bag! You'll be playing against the computer so select" +
                 "your option wisely!\n1 For Pencil\n2 For Notebook\n3 For Book Bag");
         usersChoice = input.nextLine();
-        int usersObject = Integer.parseInt(usersChoice);
+        usersObject = Integer.parseInt(usersChoice);
         if(usersObject == 1) {
-            objectName = "Pencil";
+            System.out.println("\nYou chose Pencil and the computer chose "+Object[Generator]);
         } else if(usersObject == 2) {
-            objectName = "Notebook";
+            System.out.println("\nYou chose Notebook and the computer chose "+Object[Generator]);
         } else if(usersObject == 3) {
-            objectName = "Book Bag";
+            System.out.println("\nYou chose Book Bag and the computer chose "+Object[Generator]);
         }
-        
-        System.out.println("/nYou chose "+objectName+" and the computer chose "+Object[Generator]);
         
         //Results if user chooses Pencil
         if(usersObject == 1 && Object[Generator] == Object[0]) {
@@ -49,26 +50,62 @@ public class PNB {
 
         //Results if user chooses Notebook
         if(usersObject == 2 && Object[Generator] == Object[0]) {
-            System.out.println("Notebook beats Pencil! Congrats feel free to play again.");
-        } else if(usersObject == 1 && Object[Generator] == Object[1]){
+            System.out.println("\nNotebook beats Pencil! Congrats feel free to play again.");
+        } else if(usersObject == 2 && Object[Generator] == Object[1]){
             System.out.println("\nTie! No one won feel free to play again.");
-        } else if(usersObject == 1 && Object[Generator] == Object[2]){
+        } else if(usersObject == 2 && Object[Generator] == Object[2]){
             System.out.println("\nBook Bag beats Notebook! Sorry feel free to play again.");
         }
         
         //Results if user chooses Book Bag
         if(usersObject == 3 && Object[Generator] == Object[0]) {
-            System.out.println("Pencil beats Book Bag! Sorry feel free to play again");
-        } else if(usersObject == 1 && Object[Generator] == Object[1]){
+            System.out.println("\nPencil beats Book Bag! Sorry feel free to play again");
+        } else if(usersObject == 3 && Object[Generator] == Object[1]){
             System.out.println("\nBook Bag beats Notebook! Congrats feel free to play again.");
-        } else if(usersObject == 1 && Object[Generator] == Object[2]){
+        } else if(usersObject == 3 && Object[Generator] == Object[2]){
             System.out.println("\nTie! No one won feel free to play again.");
         }
         
         boolean Loop = true;
         while(Loop){
-            System.out.println("Let's play again! You know the drill\n1 For Pencil\n2 For Notebook\n3 For Book Bag");
+            loopGenerator = PNB.nextInt(3);
+            System.out.println("\nYou know the drill\n1 For Pencil\n2 For Notebook\n3 For Book Bag");
             usersChoice = input.nextLine();
+            usersObject = Integer.parseInt(usersChoice);
+            if(usersObject == 1) {
+                System.out.println("\nYou chose Pencil and the computer chose "+Object[loopGenerator]);
+            } else if(usersObject == 2) {
+                System.out.println("\nYou chose Notebook and the computer chose "+Object[loopGenerator]);
+            } else if(usersObject == 3) {
+                System.out.println("\nYou chose Book Bag and the computer chose "+Object[loopGenerator]);
+            }
+
+            //Results if user chooses Pencil
+            if(usersObject == 1 && Object[loopGenerator] == Object[0]) {
+                System.out.println("\nTie! No one won feel free to play again.");
+            } else if(usersObject == 1 && Object[loopGenerator] == Object[1]){
+                System.out.println("\nNotebook beats Pencil! Sorry feel free to play again.");
+            } else if(usersObject == 1 && Object[loopGenerator] == Object[2]){
+                System.out.println("\nPencil beats Book Bag! Congrats feel free to play again.");
+            }
+
+            //Results if user chooses Notebook
+            if(usersObject == 2 && Object[loopGenerator] == Object[0]) {
+                System.out.println("\nNotebook beats Pencil! Congrats feel free to play again.");
+            } else if(usersObject == 2 && Object[loopGenerator] == Object[1]){
+                System.out.println("\nTie! No one won feel free to play again.");
+            } else if(usersObject == 2 && Object[loopGenerator] == Object[2]){
+                System.out.println("\nBook Bag beats Notebook! Sorry feel free to play again.");
+            }
+
+            //Results if user chooses Book Bag
+            if(usersObject == 3 && Object[loopGenerator] == Object[0]) {
+                System.out.println("\nPencil beats Book Bag! Sorry feel free to play again");
+            } else if(usersObject == 3 && Object[loopGenerator] == Object[1]){
+                System.out.println("\nBook Bag beats Notebook! Congrats feel free to play again.");
+            } else if(usersObject == 3 && Object[loopGenerator] == Object[2]){
+                System.out.println("\nTie! No one won feel free to play again.");
+            }
         }
 
     }
